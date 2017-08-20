d39141bfc1c6934ff2223928b85372f1eb91e2d2
Nicolas <nicofrati@gmail.com>
2017-08-20 20:31:10 -0300

    Version beta elbo
    A sergio no le gusta el nombre, pero va igual

---
+++ elgg/mod/tap_subir_ejercicio/Formulario.php
@@ -0,0 +1,82 @@
+<?php
+include('/var/www/html/run/config/DataBase.php');
+        $user = elgg_get_logged_in_user_entity();
+		$id = $_REQUEST['numero'];
+		//$form = Formulario::getFormulario($num);  
+		$guid = $_SESSION[_sf2_attributes][guid];
+		$user = get_user($guid);
+		//$ejer = Ejercicio::getEjercicio($id);
+		$userProb = get_user($ejer['guid_us']);		
+		try{
+			$mdb = DataBase::getDb('mysql', 'elggadd', 2);
+			$query = "SELECT * FROM ejercicios WHERE id_ex = $id";
+			$temp = $mdb->prepare($query);
+			$temp->execute();
+			$fila = $temp->fetchAll();
+			
+			$ejer = array('id_ex' => $fila[0]['id_ex'], 
+						'descripcion'=>$fila[0]['descripcion'],
+						'nombre'=>$fila[0]['nombre'], 
+						'guid_us'=> (int) $fila[0]['guid_us']);
+			$mdb = null;
+		} catch(PDOException $e){
+			print "ERROR!". $e->getMessage();
+			die();
+		}
+		$form =
+		"
+			<body>
+
+			<style>div{align-content: center;}</style>
+			<form action='http://192.168.1.13/run/runSave.php' method='post' enctype='multipart/form-data'>
+				<div align='center'>
+					<b>Ejercicio ".$ejer['nombre'].
+					"</b><br><br>"
+					.$ejer['descripcion'].
+					"<br><br>
+					Seleccione lenguaje: 
+					<br>
+					
+					<select name='language' class='elgg-button elgg-button-special elgg-button-dropdown'>
+						<option value='C'>C</option>
+						<option value='Java'>Java</option>
+					</select>
+					
+					<br><br><br>
+					Seleccione el archivo: 
+					<br>
+
+					<input type='file' name='sourcefile' class='elgg-button elgg-button-special'>
+				
+					
+					<br/>
+					<input type='hidden' id='problem' name='problem' value=".$_GET['numero'].">
+					<input type='hidden' id='usernumber' name='usernumber' value=".$guid.">
+				</div>
+						<button style='float:left;' type='submit' value='Enviar' class='elgg-button elgg-button-submit' >
+							Enviar
+						</button>
+						<button style='float: right;' type='button' value='Cancelar'  onclick='window.location=\"/elgg/tap_sol\";' class='elgg-button elgg-button-cancel'>Cancelar</button>
+
+
+
+				
+			</form>
+			<br/>
+			<br/>
+			<br/>
+			<div align='center'>
+			<form action='http://192.168.1.13/run/template.php' method='post' >
+				Descargar plantilla
+					<select name='language' class='elgg-button elgg-button-special elgg-button-dropdown'>
+						<option value='C'>C</option>
+						<option value='Java'>Java</option>
+					</select>
+					<input type='hidden' id='problem' name='problem' value=".$ejer['nombre'].">
+				<button type='submit'>Descargar</button>
+					
+			</form>
+			</div>
+
+			</body>";
+?>
